f3c7c1428ee238799050c20edd56f8878fd33b14
wddwq <70619810+wddwq@users.noreply.github.com>
2023-08-30 14:23:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+UUID='bbbf7dbe-46fd-11ee-be56-0242ac120002'
+VMESS_WSPATH='/wddvm'
+VLESS_WSPATH='/wddvl'
+TROJAN_WSPATH='/wddtr'
+SS_WSPATH='/wddsss'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
